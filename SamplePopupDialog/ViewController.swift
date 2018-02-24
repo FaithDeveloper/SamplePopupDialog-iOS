11ff3b54daf66c85b7fe0392ecd5a08a8ee95f88
신규찬 <1205kuchan@naver.com>
2018-02-24 14:10:42 +0900

    Initial Commit

---
+++ SamplePopupDialog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SamplePopupDialog
+//
+//  Created by 신규찬 on 2018. 2. 24..
+//  Copyright © 2018년 신규찬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
